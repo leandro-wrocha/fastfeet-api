45c94e5d0dcb927c5c19e59215855000adce8546
Leandro Rocha <leandro.wrocha98@gmail.com>
2024-07-20 21:14:30 -0300

    doc: setup do vitest para testes unit e e2e

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -1,23 +1,15 @@
 import swc from 'unplugin-swc';
 import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   test: {
     include: ['**/*.e2e-spec.ts'],
     globals: true,
-    alias: {
-      '@src': './src',
-      '@test': './test',
-    },
     root: './',
   },
-  resolve: {
-    alias: {
-      '@src': './src',
-      '@test': './test',
-    },
-  },
   plugins: [
+    tsconfigPaths(),
     swc.vite({
       module: { type: 'es6' },
     }),
