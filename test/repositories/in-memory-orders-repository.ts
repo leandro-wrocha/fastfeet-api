e9c8c93ea022cf177dbf35c22ae416adab844b88
Leandro Rocha <leandro.wrocha98@gmail.com>
2024-07-21 11:51:31 -0300

    feat: implementado recursos dentro do repositorio de teste

--- test/repositories/in-memory-orders-repository.ts
+++ test/repositories/in-memory-orders-repository.ts
@@ -4,7 +4,35 @@ import { Order } from '@src/domain/order/enterprise/entities/order';
 export class InMemoryOrdersRepository implements OrdersRepository {
   public items: Order[] = [];
 
+  async findMany(): Promise<Order[]> {
+    const orders = this.items;
+
+    return orders;
+  }
+
+  async findById(id: string): Promise<Order | null> {
+    const order = this.items.find((item) => item.id.toString() === id);
+
+    if (!order) {
+      return null;
+    }
+
+    return order;
+  }
+
   async create(order: Order): Promise<void> {
     this.items.push(order);
+  }
+
+  async save(order: Order): Promise<void> {
+    const orderIndex = this.items.findIndex((item) => item.id.equals(order.id));
+
+    this.items[orderIndex] = order;
+  }
+
+  async delete(order: Order): Promise<void> {
+    const orderIndex = this.items.findIndex((item) => item.id.equals(order.id));
+
+    this.items.splice(orderIndex, 1);
   }
 }
