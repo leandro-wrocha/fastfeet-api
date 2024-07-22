c0d23d065dca462ca1c83e80418961ee1328c680
Leandro Rocha <leandro.wrocha98@gmail.com>
2024-07-21 21:31:10 -0300

    fix: atualizado importacao da classe optional

--- src/domain/order/enterprise/entities/order.ts
+++ src/domain/order/enterprise/entities/order.ts
@@ -1,6 +1,6 @@
 import { AggregateRoot } from '@src/core/entities/aggregate-root';
 import { UniqueEntityID } from '@src/core/entities/unique-entity-id';
-import { Optional } from '@src/core/types/optinonal';
+import { Optional } from '@src/core/types/optional';
 
 export interface OrderProps {
   id?: UniqueEntityID;
